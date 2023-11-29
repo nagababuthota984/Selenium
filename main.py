40502f77749f28a40115515f5fa0e30b86180a36
SenaSowseelya Vishnumolakala <senasowseelya.v@Technovert.com>
2023-11-29 14:03:39 +0530

    changed drivername

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome()
-driver.get("https://www.tezo.com")
+webDriver= webdriver.Chrome()
+webDriver.get("https://www.tezo.com")
 
-element = driver.find_element("css selector", "h2")
+element = webDriver.find_element("css selector", "h2")
 print("123" + element.text)
 
-driver.quit()
+webDriver.quit()
