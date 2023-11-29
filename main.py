59887311e3546f10fbcb12533072833315252a38
SenaSowseelya Vishnumolakala <senasowseelya.v@Technovert.com>
2023-11-29 13:09:53 +0530

    change-2

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ driver = webdriver.Chrome()
 driver.get("https://www.tezo.com")
 
 element = driver.find_element("css selector", "h2")
-print("Content from h1 selector" + element.text)
+print("abc" + element.text)
 
 driver.quit()
