b64f0130e9c10b269dd8e8654d2fcd227e08a37b
SenaSowseelya Vishnumolakala <senasowseelya.v@Technovert.com>
2023-11-29 14:31:01 +0530

    from branch-a

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ webDriver= webdriver.Chrome()
 webDriver.get("https://www.tezo.com")
 
 element = webDriver.find_element("css selector", "h2")
-print("123" + element.text)
+print("This message is changes in branch-a" + element.text)
 
 webDriver.quit()
